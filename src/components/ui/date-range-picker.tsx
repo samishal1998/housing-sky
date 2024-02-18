d08728c5995f22c2b9bb58c2b7d661e48cb8ac36
samishal1998 <samishal.1998@gmail.com>
2024-02-18 02:15:56 +0200

    feat(ui): ported DatePicker from shadcn and fixed its behaviour regarding contiguous date range selection

---
+++ src/components/ui/date-range-picker.tsx
@@ -0,0 +1,92 @@
+'use client';
+
+import * as React from 'react';
+import { format } from 'date-fns';
+import { AlertTriangleIcon, Calendar as CalendarIcon } from 'lucide-react';
+import { DateRange, Matcher } from 'react-day-picker';
+
+import { cn } from '~/utils';
+import { Button } from '~/components/ui/button';
+import { Calendar } from '~/components/ui/calendar';
+import { Popover, PopoverContent, PopoverTrigger } from '~/components/ui/popover';
+import { toast } from 'sonner';
+import { checkIfRangeOverlapsMatchers } from '~/helpers/check-if-range-overlaps-matchers';
+
+export function DatePickerWithRange({
+	date,
+	onDatesChanged,
+	disabled,
+	className,
+}: React.HTMLAttributes<HTMLDivElement> & {
+	date?: DateRange;
+	disabled?: Matcher[];
+	onDatesChanged?: (dateRange?: DateRange) => void;
+}) {
+	return (
+		<div className={cn('grid gap-2', className)}>
+			<Popover>
+				<PopoverTrigger asChild>
+					<Button
+						id="date"
+						variant={'outline'}
+						className={cn(
+							'w-[300px] justify-start text-left font-normal',
+							!date && 'text-muted-foreground',
+						)}>
+						<CalendarIcon className="mr-2 h-4 w-4" />
+						{date?.from ? (
+							date.to ? (
+								<>
+									{format(date.from, 'LLL dd, y')} -{' '}
+									{format(date.to, 'LLL dd, y')}
+								</>
+							) : (
+								format(date.from, 'LLL dd, y')
+							)
+						) : (
+							<span>Pick a date</span>
+						)}
+					</Button>
+				</PopoverTrigger>
+				<PopoverContent className="w-auto p-0" align="start">
+					<Calendar
+						initialFocus
+						mode="range"
+						disabled={disabled}
+						defaultMonth={date?.from}
+						selected={date}
+						onSelect={(range, selectedDay, activeModifiers, e) => {
+							if (
+								disabled &&
+								range &&
+								checkIfRangeOverlapsMatchers(disabled, range)
+							) {
+								toast(
+									<>
+										{' '}
+										<AlertTriangleIcon
+											className={'text-red-500'}
+											strokeWidth={2}
+											size={28}
+										/>{' '}
+										Please Select Continuous Range
+									</>,
+								);
+							} else onDatesChanged?.(range);
+						}}
+						numberOfMonths={2}
+					/>
+					<div className={'p-3'}>
+						<Button
+							onClick={() => onDatesChanged?.({ from: undefined, to: undefined })}
+							variant={'ghost'}>
+							{' '}
+							Reset{' '}
+						</Button>
+					</div>
+				</PopoverContent>
+			</Popover>
+		</div>
+	);
+}
+
---
+++ src/helpers/check-if-range-overlaps-matchers.tsx
@@ -0,0 +1,20 @@
+import {DateRange, Matcher} from 'react-day-picker';
+import {areIntervalsOverlapping, Interval} from 'date-fns';
+
+export function checkIfRangeOverlapsMatchers(matchers: Matcher[], range: DateRange) {
+    if (!(range.to && range.from)) return false;
+
+    for (const matcher of matchers) {
+        let interval: Interval<Date> | undefined = undefined;
+        if (typeof matcher === 'boolean') {
+            if (matcher) interval = {start: new Date(), end: new Date()};
+        } else if (Array.isArray(matcher) && matcher[0] && matcher[1]) {
+            interval = {start: matcher[0], end: matcher[1]};
+        } else if ('from' in matcher && 'to' in matcher && matcher.from && matcher.to) {
+            interval = {start: matcher.from, end: matcher.to};
+        }
+        if (!interval) continue;
+        if (areIntervalsOverlapping(interval, {start: range.from, end: range.to})) return true;
+    }
+    return false;
+}
