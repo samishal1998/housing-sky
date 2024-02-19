3f2617d9d03a2b738372f3ccfad57566867035c4
samishal1998 <samishal.1998@gmail.com>
2024-02-19 05:38:16 +0200

    feat(hooks): added useUpdateReducer Hook, allows you to update states of type object with resetting the whole object

---
+++ src/hooks/useUpdateReducer.ts
@@ -0,0 +1,16 @@
+import React, {useCallback, useState} from "react";
+
+export function useUpdateReducer<T>(initialState: T | (()=>T)) {
+
+	const [state, setState] = useState<T>(initialState);
+
+	const updateState = useCallback((updated?: any) => {
+		setState((prevState) => ({...prevState, ...updated}))
+	}, [setState])
+
+	return ([state, setState, updateState]) as UpdateReducerTuple<T>
+
+}
+
+type UpdateReducerTuple<T> = [T, React.Dispatch<React.SetStateAction<T>>, (updated: T) => void]
+
