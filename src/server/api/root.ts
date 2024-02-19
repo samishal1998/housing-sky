3a31ba7b8148cac34dd429244110d5ca52c21374
samishal1998 <samishal.1998@gmail.com>
2024-02-19 15:56:48 +0200

    feat(api): routed all the routers to the app router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,9 @@
-import { postRouter } from "~/server/api/routers/post";
+
 import { createTRPCRouter } from "~/server/api/trpc";
+import {roomsRouter} from "~/server/api/routers/rooms";
+import {usersRouter} from "~/server/api/routers/user";
+import {hotelsRouter} from "~/server/api/routers/hotel";
+import {guestRouter} from "~/server/api/routers/guest";
 
 /**
  * This is the primary router for your server.
@@ -7,9 +11,10 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
-  user: postRouter,
-  housingListing: postRouter,
+  users:  usersRouter,
+  hotels:  hotelsRouter,
+  guest:  guestRouter,
+  rooms:  roomsRouter,
 });
 
 // export type definition of API
